f77c6fd8d1cfbd67b3582c91dfa2af9beb4e37e4
dianaxime <35496688+dianaxime@users.noreply.github.com>
2022-03-02 17:01:45 -0600

    WIP

--- secuencial.c
+++ secuencial.c
@@ -15,10 +15,19 @@
 
 #define c 1e-5
 #define L 10
+#define C 0.5
+
+double calcularTj(double TjAnt, double TjAct, double TjSig){
+	double Tj = TjAct + C * (TjAnt - 2 * TjAct + TjSig);
+	
+	return Tj;
+}
+
 
 int main(int argc, char *argv[]) {
-    double err, T0, TL, TR, dx, dt, errCalculado;
-    int N;
+    double err, T0, TL, TR, dx, dt, sum;
+    int N, n = 0;
+    double errCalculado = 1e-25;
 
     printf("--- Ingrese los parámetros para iniciar la simulación --- \n");
     printf("Precisión o diferencia requerida: ");
@@ -35,9 +44,9 @@ int main(int argc, char *argv[]) {
     double temperatura[N], temperaturaCopy[N];
     
     // Calcular dx, dt
-    dx = (L * 1.0) / N;
+    // dx = (L * 1.0) / N;
     // dt = pow(dx, 2) / c;
-    dt = 0.5 * pow(dx, 2) / c;
+    // dt = 0.5 * pow(dx, 2) / c;
     
     // Asignar los valores iniciales
     temperatura[0] = TL;
@@ -46,9 +55,35 @@ int main(int argc, char *argv[]) {
     temperaturaCopy[0] = TL;
     temperaturaCopy[N - 1] = TR;
     
+    // Inicializar array
     for (int i = 1; i < N - 1; i++) {
     	temperatura[i] = T0;
     }
+    
+    // TODO: Validar el calculo del err
+    while (errCalculado < err || n < 10) {
+    	// Calcular las temperaturas
+    	for(int j = 1; j < N - 1; j++) {
+			temperaturaCopy[j] = calcularTj(temperatura[j - 1], temperatura[j], temperatura[j + 1]);
+			//printf("%lf \t", temperaturaCopy[j]);
+		}
+		
+		// Actualizar array de solucion y calcular el err actual
+		sum = 0.0;
+		for (int k = 1; k < N - 1; k++) {
+			sum += temperaturaCopy[k] - temperaturaCopy[k - 1];
+			//printf("%lf \t", (temperaturaCopy[k] - temperaturaCopy[k - 1]));
+			temperatura[k] = temperaturaCopy[k];
+		}
+		
+		errCalculado = sum / N;
+		n += 1;
+    }
+    printf("%lf \t", errCalculado);
+    // Mostrar resultados
+    for (int i = 0; i < N; i++) {
+		printf("%lf \t", temperatura[i]);
+	}
 }
 
 
