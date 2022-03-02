e4eab2196d04229f9ca97890f3c8405c2621f1e0
dianaxime <35496688+dianaxime@users.noreply.github.com>
2022-03-02 15:41:35 -0600

    WIP

--- secuencial.c
+++ secuencial.c
@@ -12,14 +12,36 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#define c 1e-5
+
 int main(int argc, char *argv[]) {
     double err, T0, TL, TR;
     int N;
 
-    printf("--- Ingrese los parámetros para iniciar la simulación ---");
-    scanf("Precisión o diferencia requerida: %f", &err);
-    scanf("Número de intervalos discretos: %d", &N);
-    scanf("Temperatura inicial de toda la barra: %f", &T0);
-    scanf("Temperatura en la frontera izquierda (x=0): %f", &TL);
-    scanf("Temperatura en la frontera derecha: %f", &TR);
+    printf("--- Ingrese los parámetros para iniciar la simulación --- \n");
+    printf("Precisión o diferencia requerida: ");
+    scanf("%lf", &err);
+    printf("Número de intervalos discretos: ");
+    scanf("%d", &N);
+    printf("Temperatura inicial de toda la barra:");
+    scanf("%lf", &T0);
+    printf("Temperatura en la frontera izquierda (x=0): ");
+    scanf("%lf", &TL);
+    printf("Temperatura en la frontera derecha (x=L):");
+    scanf("%lf", &TR);
+    
+    double temperatura[N], temperaturaCopy[N];
+    
+    // Asignar los valores iniciales
+    temperatura[0] = TL;
+    temperatura[N - 1] = TR;
+    
+    temperaturaCopy[0] = TL;
+    temperaturaCopy[N - 1] = TR;
+    
+    for (int i = 1; i < N - 1; i++) {
+    	temperatura[i] = T0;
+    }
 }
+
+
